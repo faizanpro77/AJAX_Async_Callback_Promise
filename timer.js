cc207b7462edfa5b55d449d1bd0dfdf2c488a4ef
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-14 14:39:31 +0530

    ADDED: Ability to view Employee Data from JSON Server having ID, Name and Salary using AJAX

--- timer.js
+++ timer.js
@@ -10,3 +10,4 @@ function showSessionExpire() {
 console.log("Activity-A: Triggering Activity-B at " + showTime());
 setTimeout(showSessionExpire, 5000);
 console.log("Activity-A: Triggered Activity-B at "+ showTime() + " will execute after 5 seconds");
+
